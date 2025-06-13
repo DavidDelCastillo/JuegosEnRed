caa2e6c7b8918580d589991a9c538b2d423718e6
Fefpsy17 <daviddce04@gmail.com>
2025-06-13 19:50:24 +0200

    Preparación para usar websockers

--- backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableWebSocket
+public class DemoApplication implements WebSocketConfigurer {
 
+	@Override
+	public void registerWebSocketHandlers(
+		WebSocketHandlerRegistry registry) {
+		registry.addHandler(echoHandler(), "/echo")
+		.setAllowedOrigins("*");
+	}
+	@Bean
+	public WebsocketEchoHandler echoHandler() {
+		return new WebsocketEchoHandler();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
