c8c2a3c5f8f12b99eec19426f45f6633944d955a
Daniel Duque <117591069+Daniii18@users.noreply.github.com>
2025-06-16 15:33:32 +0200

    Update intro.js

--- backend/demo/src/main/resources/static/js/intro.js
+++ backend/demo/src/main/resources/static/js/intro.js
@@ -1,105 +1,105 @@
-class IntroScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'IntroScene' });
+class IntroScene extends Phaser.Scene{
+    constructor(){
+        super({key: 'IntroScene'});
     }
 
-    preload() {
-        // Carga de audios
+    
+
+    preload(){
+        // carga de audios
         this.load.audio("boton", 'assets/Clickar_Boton.wav');
         this.load.audio("musicaFondo", 'assets/musicMenu.mp3');
         this.load.image("chat", 'assets/Boton_Chat.png');
 
-        // Carga de imágenes
+        //carga de imágenes
         this.load.image("fondo", 'assets/menu.png');
         this.load.image("libro", 'assets/Libro.png');
         this.load.image("sombraLibro", 'assets/SombraLibro.png');
         this.load.image("periodicoM", 'assets/Menu_inicialPeri.png');
-
-        $(document).ready(function () {
-            console.log("El DOM está cargado");
-        });
+        $(document).ready(function(){
+            console.log("El DOM está cargado")
+            // Acciones sobre el documento
+            });
+            
     }
 
-    create() {
-        const centerX = this.scale.width / 2;
-        const centerY = this.scale.height / 2;
-
-        // WebSocket para iniciar partidas
-        this.socket = new WebSocket("ws://localhost:8080/ws/matchmaking");
-
-        // Música de fondo
+    create(){
+        //variables para meter las imagenes a posteriori
+        const centerX = this.scale.width/2;
+        const centerY = this.scale.height/2;
+        
+        // activacion de sonidos
         if (!this.sound.get('musicaFondo')) {
             this.music = this.sound.add("musicaFondo", { loop: true, volume: 0.5 });
             this.music.play();
             this.registry.set("musicaFondo", this.music);
-        } else {
+        } else {//Si estamos en otra escena y volvemos no se vuelve a poner la música que ya estaba, se deja como está
             this.music = this.sound.get('musicaFondo');
         }
 
-        // Fondo y elementos del menú
-        this.add.image(centerX, centerY, "fondo");
+        // montaje de la escena
+        const background_menu = this.add.image(centerX,centerY, "fondo");
 
-        this.add.text(0.1 * centerX, 0.05 * centerY, 'Mystery Mice', {
+        //Texto con el título
+        const title = this.add.text(0.1*centerX, 0.05*centerY, 'Mystery Mice', {
             font: '200px mousy',
             color: '#CDC1BF',
             backgroundColor: '#42240e80',
             align: 'center'
         });
 
-        this.add.image(0.618 * centerX, 1.2 * centerY, "sombraLibro");
-        this.add.image(0.65 * centerX, 1.2 * centerY, "libro");
-        this.add.image(centerX, centerY, "periodicoM");
 
-        // Botón de chat
-        this.add.image(1.85 * centerX, 1.8 * centerY, 'chat')
-            .setScale(0.3)
+        // interfaz del libro
+        const sombraLibro = this.add.image(0.618*centerX, 1.2*centerY, "sombraLibro");
+        const libro = this.add.image(0.65*centerX, 1.2*centerY, "libro");
+        
+        //Imagen del periodico
+        this.periodicoM = this.add.image(centerX, centerY, "periodicoM");
+
+        //boton del chat
+        const chatButton = this.add.image(1.85*centerX, 1.8*centerY, 'chat').setScale(0.3)
             .setInteractive()
-            .on('pointerdown', () => {
+            .on('pointerdown', () =>{
                 $('#chat-container').toggle();
-            });
+        });
 
-        // Botón "Empezar"
-        this.add.text(0.72 * centerX, 0.65 * centerY, 'Empezar', {
+        // botones del menú
+        //Botón para iniciar el juego
+        const startText = this.add.text(0.72*centerX, 0.65*centerY, 'Empezar',{
             font: '70px mousy',
             color: '#42240e',
             align: 'center'
         }).setInteractive()
-            .on('pointerdown', () => {
-                this.socket.send("joinQueue");
-                this.waitingText.setText("Esperando usuarios ...");
-                this.sound.play("boton");
-            });
+        .on('pointerdown', ()=>{
+            this.scene.stop("IntroScene");
+            this.scene.start("PreviewScene");            
+            this.sound.play("boton");
+        });
 
-        // Texto de espera de partida
-        this.waitingText = this.add.text(0.32*centerX, 0.52*centerY + 300, '', {
-            font: '50px mousy',
-            color: '#F0FFFF'
-        }).setOrigin(0.5);
-
-        // Botón "Controles"
-        this.add.text(0.72 * centerX, 0.9 * centerY, 'Controles', {
+        //Botón para cambiar de escena a la de los controles
+        const textControl = this.add.text(0.72*centerX, 0.9*centerY, 'Controles', {
             font: '70px mousy',
             color: '#42240e',
             align: 'center'
         }).setInteractive()
-            .on('pointerdown', () => {
-                this.scene.pause("IntroScene");
-                this.scene.launch('ControlScene', { callingScene: this.scene.key });
-                this.sound.play("boton");
-            });
+        .on('pointerdown', () =>{
+            this.scene.pause("IntroScene");
+            this.scene.launch('ControlScene', { callingScene: this.scene.key });
+            this.sound.play("boton");
+        });
 
-        // Botón "Créditos"
-        this.add.text(0.72 * centerX, 1.15 * centerY, 'Créditos', {
+        //Botón para cambiar de escena a la de créditos
+        const textCredit = this.add.text(0.72*centerX, 1.15*centerY, 'Créditos', {
             font: '70px mousy',
             color: '#42240e',
             align: 'center'
         }).setInteractive()
-            .on('pointerdown', () => {
-                this.scene.pause("IntroScene");
-                this.scene.launch('CreditScene', { callingScene: this.scene.key });
-                this.sound.play("boton");
-            });
-
+        .on('pointerdown', () =>{
+            this.scene.pause("IntroScene");
+            this.scene.launch('CreditScene', { callingScene: this.scene.key });
+            this.sound.play("boton");
+        });
+        
         // Botón "Cerrar sesión"
         this.add.text(0.66 * centerX, 1.4 * centerY, 'Cerrar Sesión', {
             font: '70px mousy',
@@ -131,22 +131,5 @@ class IntroScene extends Phaser.Scene {
             this.scene.stop("IntroScene");
             this.scene.start("LoginScene");
         });
-
-
-        // Eventos del WebSocket
-        this.socket.onmessage = (event) => {
-            if (event.data.startsWith("Esperando usuarios:")) {
-                this.waitingText.setText(event.data);
-            } else if (event.data.startsWith("startGame:")) {
-                const myRole = event.data.split(":")[1];
-                const roomId =event.data.split(":")[2];
-                this.registry.set("rol", myRole);
-                this.registry.set("room", roomId);
-                this.registry.set("socket", this.socket);
-                this.waitingText.setText("¡Partida encontrada!");
-                this.scene.stop("IntroScene");
-                this.scene.start("PreviewScene");
-            }
-        };
     }
 }
