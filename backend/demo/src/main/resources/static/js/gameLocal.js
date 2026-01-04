19fc30e5c193af0c746dee22df9128acacb41265
Daniel Duque <117591069+Daniii18@users.noreply.github.com>
2026-01-04 23:21:03 +0100

    Fuente Cronometro gameLocal.js

--- backend/demo/src/main/resources/static/js/gameLocal.js
+++ backend/demo/src/main/resources/static/js/gameLocal.js
@@ -312,7 +312,7 @@ export default class GameLoScene extends Phaser.Scene {
 
         // Crear el texto del temporizador
         this.timerText = this.add.text(0.96 * centerX, 0.52 * centerY, '00:00', {
-            font: '35px mousy',
+            font: '35px PlayfairDisplayItalic',
             color: '#FFFFFF',
         }).setScrollFactor(0);
 
